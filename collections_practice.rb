af9f80fd6efee997ed859cf447e695462744a09c
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-01 20:38:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  #should return an array sorted in ascending order
+  array.sort
+end
+def sort_array_desc(array)
+    #should return an array sorted in descending order
+ array.sort {|x,y| y <=> x}
+end
+def sort_array_char_count(array)
+  #  should return an array in ascending order sorted by the number of characters in the string
+ array.sort_by(&:length)
+end
+def swap_elements
+   #swap the second and third elements of an array
+end
+def reverse_array
+  #  reverse the order of an array of integers
+end
+def kesha_maker
+  #  taking an array as an input, change the 3rd character of each element to a dollar sign. )
+end
+  def find_a
+    #find all words that begin with "a" in the following array
 
+end
+def sum_array
+    #sum all the numbers in the following array
+
+end
+def add_s
+    #Add an "s" to each word in the array except for the 2nd element in
+end
