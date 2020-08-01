2a4c52032e4710a66a87f5e471e19e27bc503fc1
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-01 21:01:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,9 +20,7 @@ def reverse_array(array)
 end
 def kesha_maker
   #  taking an array as an input, change the 3rd character of each element to a dollar sign. )
-  array.split('').each { |3|
-      puts $
-  }
+  input.each_char {|$|}
 end
   def find_a
     #find all words that begin with "a" in the following array
